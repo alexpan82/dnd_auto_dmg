7be595d73a14e154650de00b8192691422f23251
Alex Pan <apan82@gmail.com>
2025-09-17 23:01:00 -0400

    Starting attribute matching

--- src/agent.py
+++ src/agent.py
@@ -1,18 +1,17 @@
 import os, getpass
 from langchain_openai import ChatOpenAI
-from langgraph.graph import MessagesState
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.graph import StateGraph, END
 from langchain.schema import SystemMessage
 from typing import TypedDict, Optional, List, Dict, Any, Literal
-from langgraph.checkpoint.memory import MemorySaver
-from tools import roll_dice, extract_json, add, subtract, multiply, divide
+from tools import roll_dice, extract_json, add, subtract, multiply, divide, fuzzy_match
 from langgraph.prebuilt import ToolNode, tools_condition
 from typing import Annotated, Sequence
 from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
 import sqlite3
 from langgraph.checkpoint.sqlite import SqliteSaver
+import json
 
 
 # -------- Set up OpenAI Key and Model -------- #
@@ -27,12 +26,20 @@ tools = [roll_dice, add, subtract, multiply, divide]
 llm_with_tools = llm.bind_tools(tools, parallel_tool_calls=False)
 
 
+# -------- Import relevant files -------- #
+with open('docs/character.json', 'r') as f:
+        character_json = json.load(f)
+with open('docs/weapons.json', 'r') as f:
+        weapons_json = json.load(f)
+
+
 # -------- STATE DEFINITION -------- #
 class CombatState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
     user_input: str
     parsed_action: Optional[Dict]
     character: Optional[Dict]
+    metadata: Optional[Dict]
     character_id: str
     hp: int
     status: Optional[Dict]
@@ -117,32 +124,28 @@ def parse_action(state: CombatState) -> CombatState:
 # --------- CHARACTER LOADER NODE --------- #
 # TODO: Implement fuzzy matching here
 def load_attributes(state: CombatState) -> CombatState:
+    """
+    Tries to find the corresponding characters / spells / actions / weapons
+    referenced in the user query from imported json files.
+
+    If there is a confident match, then pull that json entry into
+    calculate_damage()
+    """
     print('Matching and loading relevant JSON attributes...')
+    parsed_query_json = state['parsed_action']
+    
+    # TODO: Make a router to end the interaction if character is not found  
+    state['metadata']['character_attributes'] = fuzzy_match(parsed_query_json["character"], 
+                                                            character_json.keys())
+    
+    state['metadata']['weapon_spell_attributes'] = fuzzy_match(parsed_query_json["character"], 
+                                                               character_json.keys())
 
-    # Mock character data
-    char_db = {
-        "Avantor": {
-            "attributes": {"strength": 18},
-            "features": ["Savage Attacks", "Great Weapon Master"],
-            "inventory": {
-                "Flametongue Greatsword": {
-                    "damage": {"slashing": "2d6", "fire": "2d6"},
-                    "type": "slashing",
-                    "magic_bonus": 1
-                }
-            }
-        }
-    }
-    char_id = state["parsed_action"]["character"]
-    state["character"] = char_db.get(char_id)
-    state["character_id"] = char_id
-
-    state["status"] = {
-        "target_id": state["parsed_action"]["target_id"],
-        "resistances": ["fire"],
-        "buffs": [],
-        "homebrew_modifiers": []
-    }
+    '''
+    weapon/spell json: {weapon}
+    Character attributes: {char}
+    Character status: {status}
+    '''
     return state
 
 # --------- DAMAGE CALCULATOR NODE --------- #
@@ -150,7 +153,6 @@ def calculate_damage(state: CombatState) -> CombatState:
     print('Rolling damage 🎲 ...')
 
     char = state["character"]
-    char_id = state["character_id"]
     action = state["parsed_action"]
     status = state["status"]
     is_crit = action['is_critical_hit']
@@ -231,7 +233,6 @@ graph.add_edge("roll_dice", "calculate_damage")
 
 
 # --------- COMPILE AND RUN --------- #
-# memory = MemorySaver()
 conn = sqlite3.connect("test_checkpoints.sqlite", check_same_thread=False)
 memory = SqliteSaver(conn)
 app = graph.compile(checkpointer=memory)
